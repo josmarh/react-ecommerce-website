b599982ff4c72403bffbfcbf9eb6330c6a010671
josmarh <techjosmar@gmail.com>
2026-03-25 11:40:12 +0100

    added products to home page

---
+++ src/pages/Auth.jsx
@@ -0,0 +1,9 @@
+
+
+export default function Auth() {
+    return (
+        <div className="auth">
+            <h1>Auth Page</h1>
+        </div>
+    )
+}
---
+++ src/pages/Checkout.jsx
@@ -0,0 +1,8 @@
+
+export default function Checkout() {
+    return (
+        <div className="checkout">
+            <h1>Checkout Page</h1>
+        </div>
+    )
+}
