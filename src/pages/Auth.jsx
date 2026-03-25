78a07622d2befb9b01d2792455fe01a75fc5db05
josmarh <techjosmar@gmail.com>
2026-03-25 18:23:12 +0100

    feat: product details

--- src/pages/Auth.jsx
+++ src/pages/Auth.jsx
@@ -1,9 +1,91 @@
-
+import { useState } from "react"
+import { useForm } from "react-hook-form";
+import { useAuth } from "../context/AuthContext";
+import { useNavigate } from "react-router-dom";
 
 export default function Auth() {
+    const [mode, setMode] = useState('signup')
+    const [modeLabel, setModeLabel] = useState('Sign Up')
+    const [error, setError] = useState(null)
+
+    const { register, handleSubmit, formState: {errors} } = useForm();
+    const {signUp, login} = useAuth()
+
+    const navigate = useNavigate()
+
+    function onSubmit(data) {
+        let result;
+        if(mode === 'signup'){
+            result = signUp(data.email, data.password)
+        }else {
+            result = login(data.email, data.password)
+        }
+
+        if(result.success) {
+            navigate("/")
+        }else {
+            setError(result.error.message)
+        }
+    }
+
     return (
-        <div className="auth">
-            <h1>Auth Page</h1>
+        <div className="page">
+            <div className="container">
+                <div className="auth-container">
+                    <h1 className="page-title">{modeLabel}</h1>
+                    <form className="auth-form" onSubmit={handleSubmit(onSubmit)}>
+                        {error && <div className="error-message">{error}</div>}
+
+                        <div className="form-group">
+                            <label className="form-label" htmlFor="email">Email</label>
+                            <input 
+                            type="email" 
+                            className="form-input" 
+                            id="email"
+                            {...register("email", {required: "Email is required"})} />
+
+                            {errors.email && <p style={{ color: "red" }}>{errors.email.message}</p>}
+                        </div>
+                        <div className="form-group">
+                            <label className="form-label" htmlFor="password">Password</label>
+                            <input 
+                                type="password" 
+                                className="form-input" 
+                                id="password"
+                                {...register("password", {
+                                    required: "Password is required",
+                                    minLength: {
+                                        value: 6,
+                                        message: "Password must be at least 6 characters"
+                                    },
+                                    maxLength: {
+                                        value: 20,
+                                        message: "Password must be at most 20 characters"
+                                    }
+                                })} />
+                            {errors.password && <p style={{ color: "red" }}>{errors.password.message}</p>}
+                        </div>
+
+                        <button type="submit" className="btn btn-primary btn-large">
+                            {modeLabel}
+                        </button>
+                    </form>
+
+                    <div className="auth-switch">
+                        {mode === 'signup' ? (
+                            <p>
+                                Already have an account? {" "}
+                                <span className="auth-link" onClick={() => {setMode("login"); setModeLabel("Login")}}>Login</span>
+                            </p>
+                        ) : (
+                            <p>
+                                Don't have an account? {" "}
+                                <span className="auth-link" onClick={() => {setMode("signup"); setModeLabel("Sign Up")}}>Sign Up</span>
+                            </p>
+                        )}
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
